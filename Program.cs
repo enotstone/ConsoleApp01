f6464a080962ccad1f95984b43dc5f747b461f98
enot demiurge <enotstone@ya.ru>
2022-07-26 07:44:30 +0300

    11

--- Program.cs
+++ Program.cs
@@ -10,6 +10,24 @@ namespace ConsoleApp01
     {
         static void Main(string[] args)
         {
+            LineU equ = new LineU ();
+
+            Console.WriteLine("Для 0 = xk + b, введите b:");
+            equ.b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Для 0 = xk + b, введите k:");
+            equ.k = Convert.ToDouble(Console.ReadLine());
+            equ.Root();
+
+            Console.ReadKey();
+        }
+        struct LineU
+        {
+            public double k;
+            public double b;
+            public void Root()
+            {
+                Console.WriteLine("0 = xk + b, X = {0}", -b/k);
+            }
         }
     }
 }
